b845035f0db773325397e471350700e36b022de9
Manoj Mishra <manojmishra.deb@gmail.com>
2022-02-07 05:51:05 +0530

    Article Model

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include('can\'t be blank')
+    end
+  end
+end
